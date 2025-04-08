60a9cb3064435fe2a4b6dce9737c13e5c778de2e
Outrun <outrun.917@gmail.com>
2025-04-08 17:04:42 +0200

    0.0.2

--- js/app.js
+++ js/app.js
@@ -1,16 +1,18 @@
 const $pizzasWrapper = document.querySelector(".pizzas-wrapper");
+
 const $basketAsideSize = document.querySelector(".basket-aside-size");
+const $emptyBasket = document.querySelector(".empty-basket");
+const $basketsWithPizza = document.querySelector(".baskets-with-pizza");
+const $basketProducts = document.querySelector(".basket-products");
 
 let cartSize = 0;
+let cart = [];
 
 // Getting the products through the API
 async function getProducts() {
-  const res = await fetch("http://10.59.122.41:3000/products");
+  const res = await fetch("http://10.59.122.150:3000/products");
   // const res = await fetch("../api/products.json");
   const data = await res.json();
-
-  // console.log(res);
-  // console.log(data);
 
   createProducts(data);
 }
@@ -20,6 +22,7 @@ function createProducts(products) {
   products.forEach((product) => {
     const $newPizzaItem = document.createElement("div");
     $newPizzaItem.classList.add("pizza-item");
+    $newPizzaItem.setAttribute("data-id", product.id);
 
     const $newPizzaPicture = document.createElement("img");
     $newPizzaPicture.classList.add("pizza-picture");
@@ -47,6 +50,35 @@ function createProducts(products) {
 
     const $newAddToCartCount = document.createTextNode(1);
 
+    // Aside cart items creation
+    const $newBasketProductItem = document.createElement("li");
+    $newBasketProductItem.classList.add("basket-product-item");
+    $newBasketProductItem.setAttribute("data-name", product.name);
+    $newBasketProductItem.setAttribute("data-id", product.id);
+
+    const $newBasketProductItemName = document.createElement("span");
+    $newBasketProductItemName.classList.add("basket-product-item-name");
+    $newBasketProductItemName.textContent = product.name;
+
+    const $newBasketProductDetails = document.createElement("span");
+    $newBasketProductDetails.classList.add("basket-product-details");
+
+    const $newBasketProductDetailsQuantity = document.createElement("span");
+    $newBasketProductDetailsQuantity.classList.add(
+      "basket-product-details-quantity"
+    );
+
+    const $newBasketProductDetailsUnitPrice = document.createElement("span");
+    $newBasketProductDetailsUnitPrice.classList.add(
+      "basket-product-details-unit-price"
+    );
+    $newBasketProductDetailsUnitPrice.textContent = `@ $${product.price}.00`;
+
+    const $newBasketProductDetailsTotalPrice = document.createElement("span");
+    $newBasketProductDetailsTotalPrice.classList.add(
+      "basket-product-details-total-price"
+    );
+
     // Even listener on new add to cart btns
     $newAddToCartBtn.addEventListener("click", (e) => {
       e.preventDefault();
@@ -55,7 +87,36 @@ function createProducts(products) {
       $newAddedToCartBtn.classList.remove("hidden");
       $newPizzaPicture.classList.add("pizza-picture-active");
 
-      addCartSize();
+      cart = [
+        ...cart,
+        {
+          id: product.id,
+          name: product.name,
+          price: product.price,
+          count: 1,
+        },
+      ];
+
+      const item = cart.find((item) => item.id === product.id);
+
+      // Aside cart element handling & integration
+      $emptyBasket.classList.add("hidden");
+      $basketsWithPizza.classList.remove("hidden");
+
+      $newBasketProductItem.appendChild($newBasketProductItemName);
+      $newBasketProductItem.appendChild($newBasketProductDetails);
+      $newBasketProductDetails.appendChild($newBasketProductDetailsQuantity);
+      $newBasketProductDetails.appendChild($newBasketProductDetailsUnitPrice);
+      $newBasketProductDetails.appendChild($newBasketProductDetailsTotalPrice);
+      $basketProducts.appendChild($newBasketProductItem);
+
+      console.log(cart);
+
+      addCartSize(
+        item,
+        $newBasketProductDetailsQuantity,
+        $newBasketProductDetailsTotalPrice
+      );
     });
 
     // Event listener on remove cart item btn
@@ -68,12 +129,39 @@ function createProducts(products) {
         $newAddToCartBtn.classList.remove("add-to-cart-btn-active");
         $newPizzaPicture.classList.remove("pizza-picture-active");
 
-        substractCartSize();
+        // Aside cart element handling
+        $newBasketProductItem.remove();
+
+        const item = cart.find((item) => item.id === product.id);
+
+        // $newBasketProductDetailsQuantity.textContent = `${item.count}x`;
+
+        cart.splice(cart.indexOf(item), 1);
+
+        console.log(cart);
+
+        substractCartSize(
+          item,
+          $newBasketProductDetailsQuantity,
+          $newBasketProductDetailsTotalPrice
+        );
       } else {
         $newAddToCartCount.textContent =
           parseInt($newAddedToCartBtn.textContent) - 1;
 
-        substractCartSize();
+        const item = cart.find((item) => item.id === product.id);
+
+        // $newBasketProductDetailsQuantity.textContent = `${item.count}x`;
+
+        item.count--;
+
+        console.log(cart);
+
+        substractCartSize(
+          item,
+          $newBasketProductDetailsQuantity,
+          $newBasketProductDetailsTotalPrice
+        );
       }
     });
 
@@ -84,7 +172,17 @@ function createProducts(products) {
       $newAddToCartCount.textContent =
         parseInt($newAddedToCartBtn.textContent) + 1;
 
-      addCartSize();
+      const item = cart.find((item) => item.id === product.id);
+
+      item.count++;
+
+      console.log(cart);
+
+      addCartSize(
+        item,
+        $newBasketProductDetailsQuantity,
+        $newBasketProductDetailsTotalPrice
+      );
     });
 
     const $newCartPicture = document.createElement("img");
@@ -126,20 +224,41 @@ function createProducts(products) {
   });
 }
 
-function addCartSize() {
+function addCartSize(
+  item,
+  $newBasketProductDetailsQuantity,
+  $newBasketProductDetailsTotalPrice
+) {
   cartSize++;
   $basketAsideSize.textContent = cartSize;
-  console.log(`Cart size : ${cartSize}`);
+
+  $newBasketProductDetailsQuantity.textContent = `${item.count}x`;
+  $newBasketProductDetailsTotalPrice.textContent = `$${
+    item.price * item.count
+  }.00`;
 }
 
-function substractCartSize() {
+function substractCartSize(
+  item,
+  $newBasketProductDetailsQuantity,
+  $newBasketProductDetailsTotalPrice
+) {
+  if (cartSize === 1) {
+    $emptyBasket.classList.remove("hidden");
+    $basketsWithPizza.classList.add("hidden");
+  }
+
   if (cartSize === 0) {
     cartSize = 0;
     console.log(`Cart size is already ${cartSize}`);
   } else {
     cartSize--;
     $basketAsideSize.textContent = cartSize;
-    console.log(`Cart size : ${cartSize}`);
+
+    $newBasketProductDetailsQuantity.textContent = `${item.count}x`;
+    $newBasketProductDetailsTotalPrice.textContent = `$${
+      item.price * item.count
+    }.00`;
   }
 }
 
